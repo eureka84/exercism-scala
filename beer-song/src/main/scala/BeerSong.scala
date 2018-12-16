76dd805e71ceb113f53769546536122f94b22556
eureka84 <angelosciarra@ymail.com>
2018-12-16 22:26:18 +0100

    beer song simplified

--- beer-song/src/main/scala/BeerSong.scala
+++ beer-song/src/main/scala/BeerSong.scala
@@ -4,15 +4,14 @@ object BeerSong {
   def recite(bottles: Int, take: Int): String =
     (bottles to (bottles - take + 1) by -1).map { bottlesOnTheWall => verse(bottlesOnTheWall) }.mkString("\n")
 
-  private def verse(bottlesOnTheWall: Int) =
-    firstVerse(bottlesOnTheWall) + secondVerse(bottlesOnTheWall, bottlesOnTheWall - 1)
+  private def verse(n: Int) = firstVerse(n) + secondVerse(n)
 
   private def firstVerse(n: Int) = s"${onTheWall(n).capitalize}, ${bottles(n)} of beer.\n"
 
-  private def secondVerse(current: Int, remaining: Int): String = current match {
+  private def secondVerse(n: Int): String = n match {
     case 0 => s"Go to the store and buy some more, ${onTheWall(99)}.\n"
-    case 1 => s"Take it down and pass it around, ${onTheWall(remaining)}.\n"
-    case _ => s"Take one down and pass it around, ${onTheWall(remaining)}.\n"
+    case 1 => s"Take it down and pass it around, ${onTheWall(n - 1)}.\n"
+    case _ => s"Take one down and pass it around, ${onTheWall(n - 1)}.\n"
   }
 
   private def onTheWall(n: Int): String = s"${bottles(n)} of beer on the wall"
